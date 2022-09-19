e6619b21362a022600aeb9f17334b7911e82dbcd
Jason E. Kuipers <jason.kuipers@inl.gov>
2022-09-19 17:08:09 -0600

    fix styling on search cards

--- CSETWebNg/src/app/initial/search-page/search-page.component.ts
+++ CSETWebNg/src/app/initial/search-page/search-page.component.ts
@@ -1,10 +1,12 @@
-import { BreakpointObserver } from '@angular/cdk/layout';
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { NewAssessmentDialogComponent } from '../../dialogs/new-assessment-dialog/new-assessment-dialog.component';
 import { AssessmentService } from '../../services/assessment.service';
 import { GalleryService } from '../../services/gallery.service';
 import FuzzySearch from 'fuzzy-search';
+import { SwiperComponent } from 'swiper/angular';
+import { SwiperOptions } from 'swiper';
 
 @Component({
   selector: 'app-search-page',
@@ -12,7 +14,7 @@ import FuzzySearch from 'fuzzy-search';
   styleUrls: ['./search-page.component.scss']
 })
 export class SearchPageComponent implements OnInit, AfterViewInit  {
-
+  @ViewChild('swiper', {static: false}) swiper?: SwiperComponent;
   hoverIndex = -1;  
   searchQuery: string ='';  
   show:boolean = false;
@@ -24,15 +26,69 @@ export class SearchPageComponent implements OnInit, AfterViewInit  {
   galleryItems: any[] = [];
   galleryItemsTmp: any[] = [];
   searcher: any;
-
+  cardsPerView: number = 1;
+  rows=[];
+  config: SwiperOptions = {
+    slidesPerView: 1,
+    spaceBetween: 7,
+    slidesPerGroup: 1, 
+    
+    breakpoints: {
+      200: {
+        slidesPerView:1,
+      },
+      620:{
+        slidesPerView:2,
+      },
+      800: {
+        slidesPerView: 3, 
+      },
+      1220:{
+        slidesPerView:4,
+      },
+      1460:{
+        slidesPerView:5
+      }
+    }, 
+    on: {
+      resize: ()=>{
+        console.log(this.config.slidesPerView);
+      }
+    }
+  };
   constructor(public dialog:MatDialog, 
     public breakpointObserver: BreakpointObserver, 
     public gallerySvc: GalleryService, 
     public assessSvc: AssessmentService) { 
+      
+  
   }
 
+  ngAfterInit(){
+   
+  }
 
   ngOnInit(): void {
+
+    this.breakpointObserver.observe(['(min-width:200px)', '(min-width:620px)','(min-width:800px)', '(min-width:1220px)', '(min-width:1460px)']).subscribe((state:BreakpointState)=>{
+      if(state.breakpoints['(min-width:200px)']){
+        this.cardsPerView=1;
+      } 
+      if(state.breakpoints['(min-width:620px)']){
+        this.cardsPerView=2;
+      } 
+      if(state.breakpoints['(min-width:800px)']){
+        this.cardsPerView=3;
+      } 
+      if(state.breakpoints['(min-width:1220px)']){
+        this.cardsPerView=4;
+      } 
+      if(state.breakpoints['(min-width:1460px)']){
+        this.cardsPerView=5;
+      }
+      this.shuffelCards(this.cardsPerView);
+    });
+
     this.gallerySvc.getGalleryItems("CSET").subscribe(
       (resp: any) => {        
         resp.rows.forEach(element => {
@@ -46,14 +102,37 @@ export class SearchPageComponent implements OnInit, AfterViewInit  {
           caseSensitive: false,
           sort: false
         });
+        this.shuffelCards(this.cardsPerView);
       }
     );
-   
+    
     this.checkNavigation();
   }
   ngAfterViewInit(): void {
     this.checkNavigation();
   }
+
+  shuffelCards(i:number){
+    console.log(i);
+    this.rows = [];
+    //console.log(this.galleryItemsTmp);
+    var count=this.cardsPerView;
+    var row = [];
+    for(var x = 0; x < this.galleryItemsTmp.length - 1; x++ ){
+      if(count >= 0){
+        row.push(this.galleryItemsTmp[x])
+        count--;
+        if(count == 0)
+        {
+          this.rows.push(row);
+          count=this.cardsPerView;
+          row=[];
+        }
+      }
+    }
+    console.log(this.rows);
+  }
+
   checkNavigation(){
     let swiperPrev = document.getElementsByClassName('swiper-button-prev');
     let swiperNext = document.getElementsByClassName('swiper-button-next');
@@ -86,37 +165,15 @@ export class SearchPageComponent implements OnInit, AfterViewInit  {
   sort(searchQuery:string){        
     //this.galleryItemsTmp  = this.galleryItems.filter(x=> x.title.includes(searchQuery));    
     this.galleryItemsTmp = this.searcher.search(searchQuery);
-    console.log(this.galleryItemsTmp);
+    this.shuffelCards(this.cardsPerView);
   }
 
   onHover(i:number){
     this.hoverIndex = i;
-    if(i > 0){
-      var el = document.getElementById('c'+i.toString()).parentElement;      
-    
-    var bounding = el.getBoundingClientRect();
-    
-      let cardDimension = {x: bounding.x, y: bounding.y, w: bounding.width, h: bounding.height};
-      let viewport = {x: 0, y: 0, w: window.innerWidth, h: window.innerHeight};
-      let cardSize = cardDimension.w * cardDimension.h;
-      let xOverlap = Math.max(0, Math.min(cardDimension.x + cardDimension.w, viewport.x + viewport.w) - Math.max(cardDimension.x, viewport.x))      
-      let offScreen = cardDimension.w - xOverlap;      
-      if(offScreen > 5){
-        el.style.right = (cardDimension.w).toString() +'px';
-        
-      }
- 
-    }
   }
 
   onHoverOut(i:number, cardId: number){
-    this.hoverIndex = i;
-  
-    var el = document.getElementById('c'+cardId.toString()).parentElement;    
-    el.style.removeProperty('right');
-    
-  
-  
+    this.hoverIndex = i; 
   }
   
   getImageSrc(src: string){
